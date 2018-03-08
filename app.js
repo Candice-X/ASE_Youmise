c8f7603b4c11737395fb05d587a94ffd551a1f4c
Candice-X <494118806@qq.com>
2018-03-08 03:00:54 -0500

    modify welcome message

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+  res.status(200).send('Hello, world! Cool!').end();
 });
 
 // Start the server
