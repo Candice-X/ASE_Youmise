0f7dca59bffc9d5ebf179812244a0dec991aeced
chenKuer <chenfjk@gmail.com>
2018-03-24 15:15:49 -0400

    add account.vue, cardInfo.vue

---
+++ umise/src/components/Account.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>Account Info </h3>
+<!--   Account infomation -- >
+
+
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+};
+
+
+</script>
+
+<style>
+/*可以直接使用Bootrasp  */
+
+
+</style>
