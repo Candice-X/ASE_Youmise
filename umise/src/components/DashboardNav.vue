8314d219effd4a4e1ac2969ac31386b2c84cbb3a
chenKuer <chenfjk@gmail.com>
2018-04-23 02:02:02 -0400

    Revert "fix sign in sign up btns"
    This reverts commit c278bbbf9f55c709832bc985ed9a76672746dcda.

--- umise/src/components/DashboardNav.vue
+++ umise/src/components/DashboardNav.vue
@@ -11,8 +11,7 @@
   
     <div class="profile-usertitle-name">{{ name }}</div>
     <hr />
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
-    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
   
